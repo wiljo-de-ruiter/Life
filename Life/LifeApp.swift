d7a4c07dd9216ea2d6259cf26c72cadb803a87bc
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-01-08 20:12:32 +0100

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Wiljo de Ruiter on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
