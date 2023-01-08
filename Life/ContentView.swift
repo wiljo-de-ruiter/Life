40e652546e50703ba5a75cea3eacd6317d0a5ac6
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-01-08 22:35:36 +0100

    Initial game of life

--- Life/ContentView.swift
+++ Life/ContentView.swift
@@ -7,15 +7,185 @@
 
 import SwiftUI
 
+struct Row
+{
+    var cols: Int
+    var cell: [Bool]
+    
+    init( cols: Int )
+    {
+        self.cols = cols
+        cell = Array( repeating: false, count: cols )
+    }
+}
+
+struct Playfield
+{
+    let rows: Int
+    let cols: Int
+    
+    var data: [Row]
+    
+    init( rows: Int, cols: Int )
+    {
+        self.rows = rows
+        self.cols = cols
+        data = Array(repeating: Row(cols: cols), count: rows)
+        addVipers()
+    }
+    
+    func cell( row: Int, col: Int ) -> Bool
+    {
+        data[ row ].cell[ col ]
+    }
+    
+    mutating func setCell( row: Int, col: Int, _ val: Bool )
+    {
+        data[ row ].cell[ col ] = val
+    }
+    
+    func leftOf( _ col: Int ) -> Int
+    {
+        ( col + cols - 1 ) % cols
+    }
+    
+    func rightOf( _ col: Int ) -> Int
+    {
+        ( col + 1 ) % cols
+    }
+    
+    func above( _ row: Int ) -> Int
+    {
+        ( row + rows - 1 ) % rows
+    }
+    
+    func under( _ row: Int ) -> Int
+    {
+        ( row + 1 ) % rows
+    }
+    
+    mutating func addVipers()
+    {
+        setCell( row: 0, col: 1, true )
+        setCell( row: 1, col: 2, true )
+        setCell( row: 2, col: 0, true )
+        setCell( row: 2, col: 1, true )
+        setCell( row: 2, col: 2, true )
+
+        setCell( row: 2, col: 12, true )
+        setCell( row: 3, col: 11, true )
+        setCell( row: 4, col: 13, true )
+        setCell( row: 4, col: 12, true )
+        setCell( row: 4, col: 11, true )
+
+        setCell( row: 14, col: 2, true )
+        setCell( row: 13, col: 3, true )
+        setCell( row: 12, col: 1, true )
+        setCell( row: 12, col: 2, true )
+        setCell( row: 12, col: 3, true )
+
+        setCell( row: 22, col: 32, true )
+        setCell( row: 23, col: 31, true )
+        setCell( row: 24, col: 33, true )
+        setCell( row: 24, col: 32, true )
+        setCell( row: 24, col: 31, true )
+    }
+    
+    func computeNeighbours( row: Int, col: Int ) -> Int
+    {
+        var n = 0
+        if cell( row: row, col: leftOf( col ))      { n += 1 }
+        if cell( row: row, col: rightOf( col ))     { n += 1 }
+
+        if cell( row: above( row ), col: leftOf( col ))     { n += 1 }
+        if cell( row: above( row ), col: col )              { n += 1 }
+        if cell( row: above( row ), col: rightOf( col ))    { n += 1 }
+
+        if cell( row: under( row ), col: leftOf( col ))     { n += 1 }
+        if cell( row: under( row ), col: col )              { n += 1 }
+        if cell( row: under( row ), col: rightOf( col ))    { n += 1 }
+        
+        return n
+    }
+}
+
 struct ContentView: View {
+    @State private var field0 = Playfield(rows: 50, cols: 80)
+    @State private var field1 = Playfield(rows: 50, cols: 80)
+    @State var stopTimer = false
+    @State var cellCount = 0
+    @State var generation = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack( spacing: 1 ) {
+            HStack {
+                Button( action: {
+                    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true ) { timer in
+                        if stopTimer {
+                            timer.invalidate()
+                        } else {
+                            next()
+                        }
+                    }
+                }) {
+                    Text( "Start" )
+                }
+                .padding()
+                .padding(.horizontal)
+                .background(Color.blue)
+                .cornerRadius(25)
+                
+                Button( action: {
+                    stopTimer = true
+                }) {
+                    Text( "Stop" )
+                }
+                .padding()
+                .padding(.horizontal)
+                .background(Color.blue)
+                .cornerRadius(25)
+            }
+            .padding()
+            
+            ForEach( 0..<field0.rows, id: \.self ) { row in
+                HStack( spacing: 1 ) {
+                    ForEach( 0..<field0.cols, id: \.self ) { col in
+                        let val = field0.cell( row: row, col: col )
+                        Image( systemName: val ? "circle.fill" : "circle" )
+                            .foregroundColor( val ? Color.white : Color.gray )
+                            .font(.caption)
+                    }
+                }
+            }
+            
+            HStack {
+                Text( "Cell Count = \(cellCount)" )
+                Spacer()
+                Text( "Generation: \(generation)" )
+            }
         }
         .padding()
+    }
+    
+    func next()
+    {
+        cellCount = 0
+        for row in 0..<field0.rows {
+            for col in 0..<field0.cols {
+                let n = field0.computeNeighbours(row: row, col: col )
+                
+                if field0.cell( row: row, col: col ) {
+                    field1.setCell( row: row, col: col, n == 2 || n == 3 )
+                } else {
+                    field1.setCell( row: row, col: col, n == 3 )
+                }
+                if field1.cell( row: row, col: col ) {
+                    cellCount += 1
+                }
+            }
+        }
+        field0 = field1
+        generation += 1
     }
 }
 
